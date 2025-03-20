1eb1dd0fbbdd2bf5948466b8683df70dddfb86e1
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-03-20 13:44:52 +0100

    NFT deployed and verified correctly

--- notes.md
+++ notes.md
@@ -9,3 +9,21 @@ ERC1155 - mix between both
 ### ERC721
 
 metadata: name, description, image, attirbutes [name, values]
+
+getTokenUri: used to get the ipfs uri with the nft metadata
+
+
+### Deployments
+
+Create a script, using the forge Script package
+
+new functions needed to deploy:
+
+function run() external returns(contract){} // instance of the contract inside
+
+privateKey = vm.envUint("PRIVATE_KEY") // gets the priv key of your wallet
+vm.startBroadcast(private key) // like prank, indicates the deploying address
+
+
+Command to deploy:
+forge script [filePath] --rpc-url [rpcUrl chainlist] --broadcast --verify
--- src/firstNFT.sol
+++ src/firstNFT.sol
@@ -2,9 +2,39 @@
 
 pragma solidity >= 0.8.24;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Strings} from "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
-contract firstNFT {
+contract firstNFT is ERC721 {
+    using Strings for uint256;
 
+    uint256 public currentTokenId;
+    string public baseUri;
+    uint256 public totalSupply = 2;
+
+    event mintNFT(address account_, uint256 tokenId);
+    constructor(string memory name_, string memory symbol_, string memory _baseUri) ERC721(name_, symbol_) {
+        baseUri = _baseUri;
+    }
+
+    function mint() external {
+        require(currentTokenId < totalSupply, "All NFTs minted");
+        _safeMint(msg.sender, currentTokenId);
+
+        uint256 tokenIdMinted = currentTokenId;
+        currentTokenId++;
+
+        emit mintNFT(msg.sender, tokenIdMinted);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseUri;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireOwned(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString(), ".json") : "";
+    }
 }
